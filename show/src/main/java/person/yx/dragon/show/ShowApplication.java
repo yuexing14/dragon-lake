82ed10b47eb900808de960d52182414720aceb5a
yuexing <yuexing@g7.com.cn>
2020-04-28 18:18:21 +0800

    add show module

---
+++ show/src/main/java/person/yx/dragon/show/ShowApplication.java
@@ -0,0 +1,13 @@
+package person.yx.dragon.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowApplication.class, args);
+    }
+
+}
